8ccc17141346fc79da2e3aca4e67774f6149c505
Ali Naseri <mo.ali.naseri@gmail.com>
2022-12-10 17:33:09 +0330

    modified: add docs for Date and DateRange Classes.

--- Class-Exercises/src/tools/date/Date.java
+++ Class-Exercises/src/tools/date/Date.java
@@ -5,42 +5,89 @@ public class Date {
     private int month;
     private int day;
 
+    /**
+     * Construct a date with year, month, day values.
+     *
+     * @param year  The year number.
+     * @param month The month number.
+     * @param day   The day number
+     */
     public Date(int year, int month, int day) {
         this.year = year;
         this.month = month;
         this.day = day;
     }
 
+    /**
+     * Getting the year number of the date.
+     *
+     * @return The year value.
+     */
     public int getYear() {
         return this.year;
     }
 
+    /**
+     * Setting the year value of the date.
+     *
+     * @param year The year value.
+     */
     public void setYear(int year) {
         this.year = year;
     }
 
+    /**
+     * Getting the month number of the date.
+     *
+     * @return The month value.
+     */
     public int getMonth() {
         return this.month;
     }
 
+    /**
+     * Setting the month value of the date.
+     *
+     * @param month The month value.
+     */
     public void setMonth(int month) {
         this.month = month;
     }
 
+    /**
+     * Getting the day number of the date.
+     *
+     * @return The day value.
+     */
     public int getDay() {
         return this.day;
     }
 
+    /**
+     * Setting the day value of the date.
+     *
+     * @param day The day value.
+     */
     public void setDay(int day) {
         this.day = day;
     }
 
+    /**
+     * Checking the validity of the date.
+     *
+     * @return `true` for a valid date, `false` for a invalid date.
+     */
     public boolean isValid() {
         if (this.day < 1 || this.day > 31) return false;
         if (this.month < 1 || this.month > 12) return false;
         return this.getMonthDays() >= this.day;
     }
 
+    /**
+     * Checking whether the year is a leap year.
+     *
+     * @return `true` for leap year, `false` for non leap year.
+     */
     public boolean isLeapYear() {
         if (this.year % 400 == 0) {
             return true;
@@ -51,29 +98,58 @@ public class Date {
         return this.year % 100 != 0;
     }
 
+    /**
+     * Getting the count of days in the month of the date.
+     *
+     * @return Count of days.
+     */
     public int getMonthDays() {
         return Date.getMonthDays(this.month, this.isLeapYear());
     }
 
-    public static int getMonthDays(int month, boolean isLeapYear){
+    /**
+     * Getting the count of days in a month.
+     *
+     * @param month      The month.
+     * @param isLeapYear The year is leap or not?
+     * @return Count of days.
+     */
+    public static int getMonthDays(int month, boolean isLeapYear) {
         // we can replace this code with 'switch expression'
         switch (month) {
-            case 1, 3, 5, 7, 8, 10, 12: // These months are 31-days.
-                return 31;
+            case 4, 6, 9, 11: // These months are 30-days.
+                return 30;
             case 2: // This month is 29-days in a leap year and 28-days in a non-leap year.
                 return isLeapYear ? 29 : 28;
-            default: // other months are 30-days.
-                return 30;
+            default: // other months are 31-days.
+                return 31;
         }
     }
 
+    /**
+     * Getting the count of the days in the year of the date.
+     *
+     * @return Count of days.
+     */
     public int getYearDays() {
         return this.isLeapYear() ? 366 : 365;
     }
 
+    /**
+     * Getting the count of days between the date and another date.
+     * @param from Another date.
+     * @return Count of days between the date and the another date.
+     */
+    public int diff(Date from) {
+        return DateRange.diff(this, from);
+    }
+
+    /**
+     *
+     * @return String presentation of a date.
+     */
     @Override
     public String toString() {
         return this.year + "/" + this.month + "/" + this.day;
     }
 }
-
--- Class-Exercises/src/tools/date/DateRange.java
+++ Class-Exercises/src/tools/date/DateRange.java
@@ -1,15 +1,33 @@
 package tools.date;
 
 public class DateRange {
+    /**
+     * Finding count of previous leap years before a date.
+     *
+     * @param date A date to find its previous leap years.
+     * @return Count of leap years.
+     */
     static int countOfLeapYears(Date date) {
-        int years = date.getYear() - 1;
-        return (years / 4) - (years / 100) + (years / 400);
+        int previousYear = date.getYear() - 1;
+        return (previousYear / 4) - (previousYear / 100) + (previousYear / 400);
     }
 
+    /**
+     * Finding count of previous non-leap years before a date.
+     *
+     * @param date The date.
+     * @return Count of non-leap years.
+     */
     static int countOfNonLeapYears(Date date) {
         return date.getYear() - DateRange.countOfLeapYears(date);
     }
 
+    /**
+     * Finding all days from 0000-00-00 (origin) to the desired date.
+     *
+     * @param date The date.
+     * @return Count of days.
+     */
     static int allDays(Date date) {
         int leapYears = DateRange.countOfLeapYears(date);
         int nonLeapYears = DateRange.countOfNonLeapYears(date);
@@ -20,6 +38,12 @@ public class DateRange {
         return leapYears * 366 + nonLeapYears * 365 + monthDays + date.getDay();
     }
 
+    /**
+     *
+     * @param date1 The first date.
+     * @param date2 The second date.
+     * @return Count of days between the first date and the second date.
+     */
     public static int diff(Date date1, Date date2) {
         int date1Days = DateRange.allDays(date1);
         int date2Days = DateRange.allDays(date2);
